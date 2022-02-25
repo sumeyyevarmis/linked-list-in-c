d19907c5c34ec517ebdf4c440b7944d0fb9171b0
Süm <57527976+one-engineer@users.noreply.github.com>
2022-02-25 22:58:49 +0300

    Create single-linked-list.c

---
+++ single-linked-list.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    struct Node* next;
+};
+
+void printList(struct Node* n){
+    while(n!=NULL){
+        printf(" %d ", n->data);
+        n= n->next;
+    }
+}
+
+int main() {
+    struct Node* head = NULL;
+    struct Node* second = NULL;
+    struct Node* third = NULL;
+
+    // allocate 3 nodes in the heap - yýðýnda 3 düðüm alaný tahsis eder
+    head =(struct Node*) malloc(sizeof (struct Node));
+    second =(struct Node*) malloc(sizeof (struct  Node));
+    third = (struct Node*) malloc(sizeof (struct Node));
+
+    head->data = 1; // assign data in first node- ilk düðüme deðer atama
+    head->next = second; //  link first node with the second node-ilk düðüm ikinci düðüm ile baðlanýr
+
+    second->data =2; // assign data in second node
+    second->next = third; // node connects to other node
+
+    third->data =3; // assign data in thrid node
+    third->next = NULL;
+
+    printList(head);
+
+    puts("Terminated");
+    return 0;
+}
